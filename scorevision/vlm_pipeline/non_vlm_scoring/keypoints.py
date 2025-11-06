8dd2b263564e782e939d78d235ed0c76bb93c6c1
Mohammed Terry-Jack <mohammed@mohammed-mac-mini.local>
2025-11-06 10:27:02 +0000

    ensure 0 0 keypoints are properly ignored if not tuple

--- scorevision/vlm_pipeline/non_vlm_scoring/keypoints.py
+++ scorevision/vlm_pipeline/non_vlm_scoring/keypoints.py
@@ -125,7 +125,7 @@ def project_image_using_keypoints(
     filtered_dst = []
 
     for src_pt, dst_pt in zip(source_keypoints, destination_keypoints, strict=True):
-        if dst_pt == (0.0, 0.0):  # ignore default / missing points
+        if dst_pt[0] == 0.0 and dst_pt[1] == 0.0:  # ignore default / missing points
             continue
         filtered_src.append(src_pt)
         filtered_dst.append(dst_pt)
