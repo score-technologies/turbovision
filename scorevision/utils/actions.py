f17f2fb14e9bfcb9961fc6bfae7e2501cc80371b
Miles Kensington <miles@wearescore.com>
2026-03-19 10:21:52 +0000

    Adjusted action spotting config

--- scorevision/utils/actions.py
+++ scorevision/utils/actions.py
@@ -10,7 +10,6 @@ class Action(str, Enum):
     INTERCEPTION = "interception"
     BALL_OUT_OF_PLAY = "ball_out_of_play"
     CLEARANCE = "clearance"
-    DEFENSIVE_1ON1 = "defensive_1on1"
     TAKE_ON = "take_on"
     SUBSTITUTION = "substitution"
     BLOCK = "block"
@@ -18,10 +17,7 @@ class Action(str, Enum):
     SHOT = "shot"
     SAVE = "save"
     FOUL = "foul"
-    FOUL_WON = "foul_won"
     GOAL = "goal"
-    CATCH_INTERCEPTION = "catch_interception"
-    END_OF_HALF = "end_of_half"
 
 
 class ActionConfig(NamedTuple):
@@ -38,7 +34,6 @@ ACTION_CONFIGS: dict[Action, ActionConfig] = {
     Action.INTERCEPTION: ActionConfig(2.8, 0.5, 2.0),
     Action.BALL_OUT_OF_PLAY: ActionConfig(2.9, 0.5, 2.0),
     Action.CLEARANCE: ActionConfig(3.1, 0.5, 2.0),
-    Action.DEFENSIVE_1ON1: ActionConfig(3.2, 0.5, 2.0),
     Action.TAKE_ON: ActionConfig(3.2, 0.5, 2.0),
     Action.SUBSTITUTION: ActionConfig(4.2, 0.5, 2.0),
     Action.BLOCK: ActionConfig(4.2, 0.5, 2.0),
@@ -46,8 +41,5 @@ ACTION_CONFIGS: dict[Action, ActionConfig] = {
     Action.SHOT: ActionConfig(4.7, 0.5, 2.0),
     Action.SAVE: ActionConfig(7.3, 0.5, 2.0),
     Action.FOUL: ActionConfig(7.7, 0.5, 2.5),
-    Action.FOUL_WON: ActionConfig(7.7, 0.5, 2.5),
     Action.GOAL: ActionConfig(10.9, 0.5, 3.0),
-    Action.CATCH_INTERCEPTION: ActionConfig(17.2, 0.4, 2.5),
-    Action.END_OF_HALF: ActionConfig(17.2, 0.5, 5.0),
 }
