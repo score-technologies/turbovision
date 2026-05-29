6fed36fceab5273e8b98e10a242eecbf986cfc31
mohammedterryjack <mohammedterryjack@gmail.com>
2026-05-29 12:56:46 +0100

    scoring functions for polygons filled in

--- tests/vlm_pipeline/non_vlm_scoring/test_polygons.py
+++ tests/vlm_pipeline/non_vlm_scoring/test_polygons.py
@@ -4,13 +4,18 @@ from scorevision.vlm_pipeline.non_vlm_scoring.polygons import (
     compare_polygon_counts,
     compare_polygon_false_positive,
     compare_polygon_placement,
+    compare_polygon_map50,
     compare_polygon_precision,
     compare_polygon_recall,
 )
+from scorevision.vlm_pipeline.domain_specific_schemas.football import Action
+from scorevision.vlm_pipeline.utils.data_models import PseudoGroundTruth
+from scorevision.vlm_pipeline.utils.response_models import BoundingBox, FrameAnnotation, ShirtColor
 
 
 def test_polygon_metrics_registered():
     assert (ElementPrefix.POLYGON_DETECTION, PillarName.POLYGON_IOU) in METRIC_REGISTRY
+    assert (ElementPrefix.POLYGON_DETECTION, PillarName.MAP50) in METRIC_REGISTRY
     assert (ElementPrefix.POLYGON_DETECTION, PillarName.POLYGON_COUNT) in METRIC_REGISTRY
     assert (ElementPrefix.POLYGON_DETECTION, PillarName.POLYGON_PRECISION) in METRIC_REGISTRY
     assert (ElementPrefix.POLYGON_DETECTION, PillarName.POLYGON_RECALL) in METRIC_REGISTRY
@@ -18,8 +23,42 @@ def test_polygon_metrics_registered():
 
 
 def test_polygon_placeholder_metrics_return_zero():
-    assert compare_polygon_placement() == 0.0
-    assert compare_polygon_counts() == 0.0
-    assert compare_polygon_precision() == 0.0
-    assert compare_polygon_recall() == 0.0
-    assert compare_polygon_false_positive() == 0.0
+    gt = PseudoGroundTruth(
+        video_name="v",
+        frame_number=1,
+        spatial_image=None,  # not used by the scoring functions
+        temporal_image=None,
+        annotation=FrameAnnotation(
+            bboxes=[
+                BoundingBox(
+                    bbox_2d=(0, 0, 10, 10),
+                    polygon=[(0, 0), (10, 0), (10, 10), (0, 10)],
+                    label="player",
+                    cluster_id=ShirtColor.WHITE,
+                )
+            ],
+            category=Action.NONE,
+            confidence=100,
+            reason="test",
+        ),
+    )
+    miner = {
+        1: {
+            "polygons": [
+                BoundingBox(
+                    bbox_2d=(0, 0, 10, 10),
+                    polygon=[(0, 0), (10, 0), (10, 10), (0, 10)],
+                    label="player",
+                    cluster_id=ShirtColor.WHITE,
+                )
+            ]
+        }
+    }
+    pseudo_gt = [gt]
+
+    assert compare_polygon_placement(pseudo_gt=pseudo_gt, miner_predictions=miner) == 1.0
+    assert compare_polygon_map50(pseudo_gt=pseudo_gt, miner_predictions=miner) == 1.0
+    assert compare_polygon_counts(pseudo_gt=pseudo_gt, miner_predictions=miner) == 1.0
+    assert compare_polygon_precision(pseudo_gt=pseudo_gt, miner_predictions=miner) == 1.0
+    assert compare_polygon_recall(pseudo_gt=pseudo_gt, miner_predictions=miner) == 1.0
+    assert compare_polygon_false_positive(pseudo_gt=pseudo_gt, miner_predictions=miner) == 1.0
